61421443e742dc3da137641fbf50fa551aaa45a0
fabianoleite <fabyano.milk@gmail.com>
2012-08-10 01:52:22 -0300

    some refactoring

--- README.md
+++ README.md
@@ -5,9 +5,3 @@ This is my collection of dotfiles.
 ```bash
 git clone https://github.com/fabianoleite/dotfiles.git && cd dotfiles && ./bootstrap.sh
 ```
-
-## Thanks to…
-
-* [Lucas Húngaro](https://github.com/lucashungaro/dotfiles)
-* [Mathias Bynens](https://github.com/mathiasbynens/dotfiles)
-* [Gianni Chiappetta](https://github.com/gf3/dotfiles)
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,9 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-git pull
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-}
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
-fi
-unset doIt
+cp files/bash_profile ~/.bash_profile
+cp files/gitconfig ~/.gitconfig
+cp files/irbrc ~/.irbrc
+cp files/bashrc ~/.bashrc
+cp files/gemrc ~/.gemrc
+cp files/pryrc ~/.pryrc
+cp files/gitignore ~/.gitignore
+
 source ~/.bash_profile
